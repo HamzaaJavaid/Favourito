121d3656151280325c29d0e7f4d99d45c1a38205
Fahad Zia <76650975+mediocre9@users.noreply.github.com>
2023-02-18 20:03:10 +0500

    refactor: remove boilerplate code for `provider`

--- lib/main.dart
+++ lib/main.dart
@@ -9,45 +9,22 @@ void main(){
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
   Widget build(BuildContext context) {
     return MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_)=> list()),
-          ChangeNotifierProvider(create: (_)=> theme()),
-
-        ],
-    child: MaterialApp(
-        home: Xtra_class(),
-      ),
-
+      providers: [
+        ChangeNotifierProvider(create: (_) => list()),
+        ChangeNotifierProvider(create: (_) => theme()),
+      ],
+      builder: (context, child) {
+        return MaterialApp(
+          theme: context.watch<theme>().present_theme,
+          home: const Main_Screen(),
+        );
+      },
     );
   }
 }
-
-class Xtra_class extends StatefulWidget {
-  const Xtra_class({Key? key}) : super(key: key);
-
-  @override
-  _Xtra_classState createState() => _Xtra_classState();
-}
-
-class _Xtra_classState extends State<Xtra_class> {
-  @override
-  Widget build(BuildContext context) {
-    final providerr = Provider.of<theme>(context);
-    return MaterialApp(
-      theme: providerr.present_theme,
-      home: Main_Screen(),
-    );
-  }
-}
-
